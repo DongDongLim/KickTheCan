92c2b894a43ffdeedc6dd1d473f58bd4305b6d8e
ksbbbb <enngo98@gmail.com>
2022-06-14 21:41:30 +0900

    feat : SeekerBase

---
+++ Assets/KSB/Scripts/SeekerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+public class SeekerController : MonoBehaviourPun
+{
+    private Rigidbody rigid;
+    private Animator anim;
+    public GameObject attackColl;
+
+    private void Awake() {
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        Move();
+        Jump();
+    }
+
+    [PunRPC]
+    private void Move(){
+
+    }
+    
+    [PunRPC]
+    private void Jump(){
+
+    }
+
+    [PunRPC]
+    private void Attack(){
+        attackColl.SetActive(true);
+        StartCoroutine("AttackEnd");
+    }
+
+    IEnumerator AttackEnd(){
+        yield return new WaitForSeconds(0.1f);
+    }
+}
