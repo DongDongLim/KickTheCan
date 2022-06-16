ff49f1acbf4c7588113b4e763b31fad7b5e872cf
ksbbbb <enngo98@gmail.com>
2022-06-16 17:30:46 +0900

    fix : SeekerBase
    Attack, Jump

--- Assets/KSB/Scripts/SeekerController.cs
+++ Assets/KSB/Scripts/SeekerController.cs
@@ -1,62 +1,102 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
+
 public class SeekerController : MonoBehaviourPun
 {
-    private Rigidbody rigid;
-    private Animator anim;
-    public GameObject attackColl;
-
-    private float hAxis = 0f;
-    private float vAxis = 0f;
-    private float moveSpeed = 5f;
-    private float jumpPower = 5f;
-    private Vector3 moveVec;    // 플레이어 움직이는 방향
-
-    private bool isGrounded;		        // 땅에 서있는지 체크하기 위한 bool값
-    public float groundDistance = 0.5f;		// Ray를 쏴서 검사하는 거리
-
-    private void Awake() {
-        rigid = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
-    }
-
-    private void Update() {
-        Move();
-        Jump();
-        Attack();
-    }
-
-    [PunRPC]
-    private void Move(){
-        hAxis = Input.GetAxis("Horizontal");
-        vAxis = Input.GetAxis("Vertical");
-
-        moveVec = new Vector3(hAxis,0,vAxis).normalized;
-        transform.LookAt(transform.position + moveVec);
-
-        transform.position += moveVec * moveSpeed * Time.deltaTime;
-    }
+    [SerializeField]
+    private Transform charactorBody;
     
-    [PunRPC]
-    private void Jump(){
-        if (!isGrounded)
-            return;
+    [SerializeField]
+    private Transform cameraArm;
 
-        if (!Input.GetButtonDown("Jump"))
-            return;
-        
-        rigid.AddForce(Vector3.up * jumpPower,ForceMode.Impulse);
+    [SerializeField]
+    private GameObject attackColl;
+
+    // 컴포넌트
+    private Animator animator;
+    private Rigidbody rigid;
+
+    // 이동
+    private Vector2 moveInput;
+    private bool isMove;
+    public float moveSpeed;
+
+    // 점프
+    private bool isJump;
+    public float jumpPower;
+
+    // 카메라 이동
+    public float rotateSpeed;
+
+    private void Start() {
+        animator = charactorBody.GetComponent<Animator>() == null ? null : charactorBody.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     [PunRPC]
-    private void Attack(){
-        if (!Input.GetButtonDown("Fire1"))
+    public void Move(Vector2 inputDirection){
+        moveInput = inputDirection;
+        isMove = moveInput.magnitude != 0;
+
+        Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+        Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+        Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+            
+        animator?.SetBool("isMove", isMove);
+
+        if (!isMove)
             return;
-        
+
+        charactorBody.forward = moveDir;
+        rigid.MovePosition(transform.position + moveDir * Time.deltaTime * moveSpeed);
+    }
+
+    [PunRPC]
+    public void LookAround(Vector2 inputDirection){
+        // 마우스 이동 값 검출
+        Vector2 mouseDelta = inputDirection * rotateSpeed;
+        // 카메라의 원래 각도를 오일러 각으로 저장
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+        // 카메라의 피치 값 계산
+        float x = camAngle.x - mouseDelta.y;
+
+        // 카메라 피치 값을 위쪽으로 70도 아래쪽으로 25도 이상 움직이지 못하게 제한
+        if (x < 180f)
+        {
+            x = Mathf.Clamp(x, -1f, 70f);
+        }
+        else
+        {
+            x = Mathf.Clamp(x, 335f, 361f);
+        }
+
+        // 카메라 암 회전 시키기
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    }
+
+    [PunRPC]
+    public void Jump(){
+        if (!Input.GetButtonDown("Jump"))
+            return; 
+
+        if (isJump)
+            return;
+
+        rigid.AddForce(Vector3.up * jumpPower,ForceMode.Impulse);
+
+        animator?.SetBool("isMove", false);
+
+        isJump = true;
+        animator?.SetBool("isJump", isJump);
+    }
+
+    [PunRPC]
+    public void Attack(){
         attackColl.SetActive(true);
+        animator?.SetTrigger("isAttack");
         StartCoroutine("AttackEnd");
     }
 
@@ -65,15 +105,37 @@ public class SeekerController : MonoBehaviourPun
         attackColl.SetActive(false);
     }
 
+    private void Update() {
+        Jump();
+        GroundChecker();
+
+    }
+
+    private void GroundChecker(){
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + Vector3.up, Vector3.down,out hit,1f,LayerMask.GetMask("Ground"))){
+            isJump = false;
+            animator?.SetBool("isJump", isJump);
+            Debug.Log("땅");
+        }
+        else{
+            animator?.SetBool("isMove", false);
+
+            isJump = true;
+            animator?.SetBool("isJump", isJump);
+        }
+        Debug.DrawRay(transform.position + Vector3.up, Vector3.down * 1f, Color.yellow);
+    }
+
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "Ground"){
-            isGrounded = true;
+            
         }
+    }
+    private void OnCollisionStay(Collision other) {
+        
     }
 
     private void OnCollisionExit(Collision other) {
-        if (other.gameObject.tag == "Ground"){
-            isGrounded = false;
-        }
     }
 }
