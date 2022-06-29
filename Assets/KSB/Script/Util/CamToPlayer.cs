26d72d25c1c01a3eda913c81a1068cc1f937dfa6
ksbbbb <enngo98@gmail.com>
2022-06-29 17:16:55 +0900

    fix : Base Setting

--- Assets/KSB/Script/Util/CamToPlayer.cs
+++ Assets/KSB/Script/Util/CamToPlayer.cs
@@ -8,15 +8,46 @@ public class CamToPlayer : MonoBehaviour
     private Camera mainCam;
 
     [SerializeField]
-    private GameObject player;
+    private Transform player;
+
+    private RaycastHit[] TransparentHits;
 
     private void Start()
     {
-        player = DH.CameraMng.instance.playerObject;
+        player = DH.CameraMng.instance.playerObject.transform;
     }
 
     private void Update()
     {
         
+    }
+
+    public void CamRayToChar()
+    {
+        // 캐릭터 Pos
+        Vector3 CharPos = player.position;
+
+        // 카메라와 캐릭터의 거리
+        float Distance = Vector3.Distance(transform.position, player.transform.position);
+
+        // 카메라와 캐릭터의 방향
+        Vector3 DirToCam = (transform.position - CharPos).normalized;
+
+        TransparentHits = Physics.RaycastAll(CharPos, DirToCam, Distance);
+        Debug.DrawRay(CharPos, Distance * DirToCam, Color.red, 0.1f);
+
+        for (int i = 0; i < TransparentHits.Length; i++)
+        {
+            RaycastHit hit = TransparentHits[i];
+
+            //Debug.Log(hit.collider.gameObject.transform.parent.transform.GetComponentInChildren<MeshRenderer>().name);
+            //objectRender = 
+            //if (objectRender != null)
+            //{
+                
+
+            //    Debug.Log("렌더러 받아옴");
+            //}
+        }
     }
 }
