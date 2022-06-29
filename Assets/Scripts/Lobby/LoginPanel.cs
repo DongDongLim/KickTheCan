5f742d6332912458d7079aa73183dc03e1c8a998
DongDongLim <99638155+DongDongLim@users.noreply.github.com>
2022-06-29 21:38:00 +0900

    fix : fail Login

--- Assets/Scripts/Lobby/LoginPanel.cs
+++ Assets/Scripts/Lobby/LoginPanel.cs
@@ -1,10 +1,8 @@
+
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
-#if UNITY_ANDROID
-using GooglePlayGames;
-using GooglePlayGames.BasicApi;
-#endif
+using System.Collections;
 
 public class LoginPanel : MonoBehaviour
 {
@@ -14,11 +12,15 @@ public class LoginPanel : MonoBehaviour
 
     void Start()
     {
-#if UNITY_ANDROID
-        // 구글 플레이 서비스에 로그인되어 있지 않으면 로그인 패널을 띄움
-        if (!IsAuthenticated())
-            googleLoginBtn.SetActive(true);
-#endif
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            // 구글 플레이 서비스에 로그인되어 있지 않으면 로그인 패널을 띄움
+            if (IsAuthenticated())
+                OnLoginButtonClicked();
+            else
+                googleLoginBtn.SetActive(true);
+
+        }
     }
 
     public void OnLoginButtonClicked()
@@ -35,42 +37,10 @@ public class LoginPanel : MonoBehaviour
         PhotonNetwork.ConnectUsingSettings();
     }
 
-
-#if UNITY_ANDROID
-    
-    // 구글 플레이 서비스에 로그인이 되어있는지 확인하는 함수
-    
     public static bool IsAuthenticated()
     {
         return Social.localUser.authenticated;
     }
 
-    // 로그인 버튼을 눌렀을 때
-    public void Start_Auth()
-    {
-        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
-            //.EnableSavedGames() // 게임 진행 상황을 저장할 수 있게 함
-            //.WithInvitationDelegate(< callback method >) // 게임이 꺼져 있을 때, 게임 초대 수신을 위한 콜백
-            //.WithMatchDelegate(< callback method >) // 게임이 꺼져 있을 때, 턴 기반 매치 알림을 수신을 위한 콜백
-            //.RequestEmail() // 플레이어의 이메일 주소를 사용할 수 있도록 요청 + 동의를 요청하는 메시지를 띄움
-            //.RequestServerAuthCode( false ) // 연결된 백엔드 서버 애플리케이션에 전달되고 OAuth 토큰으로 교환될 수 있도록 서버 인증 코드를 생성하도록 요청
-            //.RequestIdToken() // ID 토큰 생성을 요청(Firebase에서 플레이어를 식별하는 데 사용)
-            .Build();
 
-        PlayGamesPlatform.InitializeInstance(config);
-        PlayGamesPlatform.DebugLogEnabled = true; // recommended for debugging
-        PlayGamesPlatform.Activate(); // Activate the Google Play Games platform
-
-        Social.localUser.Authenticate((bool success) =>
-        {
-            // handle success or failure
-            if (success)
-                OnLoginButtonClicked(); //로그인 성공 - 로그인 패널 꺼짐
-            else 
-                Debug.Log("Google Login fail"); //로그인 실패
-        });
-    }
-
-
-#endif
 }
--- Assets/YSM/Scripts/Firebase/AuthManager.cs
+++ Assets/YSM/Scripts/Firebase/AuthManager.cs
@@ -5,6 +5,10 @@ using Firebase.Auth;
 using UnityEngine.UI;
 using Photon.Pun;
 using UnityEngine.Events;
+#if UNITY_ANDROID
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+#endif
 
 public class AuthManager : MonoBehaviour
 {
@@ -76,9 +80,75 @@ public class AuthManager : MonoBehaviour
     {
         return auth.CurrentUser.UserId;
     }
-   
+
+#if UNITY_ANDROID
+
+    // 구글 플레이 서비스에 로그인이 되어있는지 확인하는 함수
+
+
+    // 로그인 버튼을 눌렀을 때
+    public void Start_Auth()
+    {
+        //PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        //    //.EnableSavedGames() // 게임 진행 상황을 저장할 수 있게 함
+        //    //.WithInvitationDelegate(< callback method >) // 게임이 꺼져 있을 때, 게임 초대 수신을 위한 콜백
+        //    //.WithMatchDelegate(< callback method >) // 게임이 꺼져 있을 때, 턴 기반 매치 알림을 수신을 위한 콜백
+        //    //.RequestEmail() // 플레이어의 이메일 주소를 사용할 수 있도록 요청 + 동의를 요청하는 메시지를 띄움
+        //    //.RequestServerAuthCode( false ) // 연결된 백엔드 서버 애플리케이션에 전달되고 OAuth 토큰으로 교환될 수 있도록 서버 인증 코드를 생성하도록 요청
+        //    .RequestIdToken() // ID 토큰 생성을 요청(Firebase에서 플레이어를 식별하는 데 사용)
+        //    .Build();
+
+        //PlayGamesPlatform.InitializeInstance(config);
+        //PlayGamesPlatform.DebugLogEnabled = true; // recommended for debugging
+        //PlayGamesPlatform.Activate(); // Activate the Google Play Games platform
+
+        PlayGamesPlatform.InitializeInstance(new PlayGamesClientConfiguration.Builder()
+            .RequestIdToken()
+            .RequestEmail()
+            .Build());
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+
+        Social.localUser.Authenticate((bool success) =>
+        {
+            // handle success or failure
+            if (success)
+            {
+                StartCoroutine(TryFirebaseLogin()); //로그인 성공 - 로그인 패널 꺼짐
+            }
+            else
+                Application.Quit();
+        });
+    }
+
+    IEnumerator TryFirebaseLogin()
+    {
+        while (string.IsNullOrEmpty(((PlayGamesLocalUser)Social.localUser).GetIdToken()))
+            yield return null;
+        string idToken = ((PlayGamesLocalUser)Social.localUser).GetIdToken();
+
+
+        Credential credential = GoogleAuthProvider.GetCredential(idToken, null);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Debug.Log("Success!");
+        });
+    }
+
+
+#endif
+
 }
 
 
 
-                                                                                                                                                 
