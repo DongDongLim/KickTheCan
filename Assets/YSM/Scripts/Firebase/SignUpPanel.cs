0e39e908672ded553d32d3bcf249cdeacc79d59b
ksbbbb <99638096+ksbbbb@users.noreply.github.com>
2022-06-30 16:11:41 +0900

    feat : Login , SignUp UI
    Login Panel UI,
    SingUp Panel UI

--- Assets/YSM/Scripts/Firebase/SignUpPanel.cs
+++ Assets/YSM/Scripts/Firebase/SignUpPanel.cs
@@ -5,35 +5,47 @@ using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using TMPro;
 using Firebase.Auth;
 
 public class SignUpPanel : MonoBehaviour
 {
     [Header("email")]
     [SerializeField] private InputField emailInputField;
+    [SerializeField] private TextMeshProUGUI emailMessage;
+    [SerializeField] private Image emailCheckImage01;
+    [SerializeField] private Image emailCheckImage02;
     bool emailCheck;
+
+    int emailChecker;
 
     [Header("password")]
     [SerializeField] private InputField passwordInputField;
-    [SerializeField] private Text passwordMessage;
+    [SerializeField] private TextMeshProUGUI passwordMessage;
     bool passwordCheck;
 
     [Header("passwordConfirm")]
     [SerializeField] private InputField passwordConfirmInputField;
-    [SerializeField] private Text passwordConfirmMessage;
+    [SerializeField] private TextMeshProUGUI passwordConfirmMessage;
     bool passwordConfirmCheck;
 
 
     [Header("NickName")]
     [SerializeField] private InputField displayNickName;
+    [SerializeField] private TextMeshProUGUI nicknameMessage;
+    [SerializeField] private Image nickCheckImage01;
+    [SerializeField] private Image nickCheckImage02;
     [SerializeField] private Button displayNicknameCheckButton;
     bool displayNicknameCheck;
 
+    int nicknameChecker;
 
 
     [SerializeField] private Button SignUpButton;
     [SerializeField] private CheckPanel CheckBox;
     [SerializeField] private SignUpComplete signUpCompletePanel;
+
+    int signUpButtonChecker;
 
 
     bool isEmailDuplication;
@@ -47,9 +59,16 @@ public class SignUpPanel : MonoBehaviour
         passwordConfirmInputField.text = "";
         displayNickName.text = "";
 
-        passwordMessage.text = "6~20 비밀번호 입력";
-        passwordConfirmMessage.text = "비밀번호 확인";
+        emailMessage.text = "";
+        passwordMessage.text = "";
+        passwordConfirmMessage.text = "";
+        nicknameMessage.text = "";
 
+        emailCheckImage01.color = Color.black;
+        emailCheckImage02.color = Color.black;
+        nickCheckImage01.color = Color.black;
+        nickCheckImage02.color = Color.black;
+        
         emailCheck = false;
         passwordCheck = false;
         passwordConfirmCheck = false;
@@ -57,6 +76,8 @@ public class SignUpPanel : MonoBehaviour
         isEmailDuplication = false;
         SignUpButton.interactable = false;
         signUpCompletePanel.gameObject.SetActive(false);
+
+        signUpButtonChecker = 0;
     }
 
 
@@ -95,28 +116,62 @@ public class SignUpPanel : MonoBehaviour
         });
     }
 
-
-
     public void EmailCheckClick()
     {
         if (isEmailDuplication)
         {
             Debug.Log("중복된 이메일 입니다");
+            emailChecker = 0;
             emailCheck = false;
         }
         else if (IsValidEmail(emailInputField.text))
         {
-            Debug.Log("정상적인 이메일입니다.");
+            Debug.Log("사용 가능한 이메일입니다");
+            emailChecker = 1;
             emailCheck = true;
         }
         else
         {
             Debug.Log("잘못된 이메일 형식 입니다");
+            emailChecker = 2;
             emailCheck = false;
         }
         SignUpInteractable();
     }
 
+#region EmailChecker
+    public void EmailChecker()
+    {
+        StartCoroutine(EmailCheckerCoroutine());
+    }
+
+    IEnumerator EmailCheckerCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        switch (emailChecker)
+        {
+            case 0:
+                Debug.Log("New0");
+                emailMessage.text = "중복된 이메일 입니다";
+                emailMessage.color = Color.red;
+                emailCheckImage01.color = Color.red;
+                emailCheckImage02.color = Color.red;
+                break;
+            case 1:
+                emailMessage.text = "사용 가능한 이메일입니다";
+                emailMessage.color = Color.green;
+                emailCheckImage01.color = Color.green;
+                emailCheckImage02.color = Color.green;
+                break;
+            case 2:
+                emailMessage.text = "잘못된 이메일 형식 입니다";
+                emailMessage.color = Color.red;
+                emailCheckImage01.color = Color.red;
+                emailCheckImage02.color = Color.red;
+                break;
+        }
+    }
+#endregion
 
 
 
@@ -141,19 +196,44 @@ public class SignUpPanel : MonoBehaviour
     {
         if (isDisplayNickNameDuplication)
         {
-            Debug.Log("중복된 닉네임");
+            nicknameChecker = 0;
             displayNicknameCheck = false;
         }
         else
         {
-            Debug.Log("사용 가능한 닉네임");
+            nicknameChecker = 1;
             displayNicknameCheck = true;
 
         }
         SignUpInteractable();
     }
 
+#region NickNameChecker
+    public void NickNameChecker()
+    {
+        StartCoroutine(NickNameCheckerCoroutine());
+    }
 
+    IEnumerator NickNameCheckerCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        switch (nicknameChecker)
+        {
+            case 0:
+                nicknameMessage.text = "중복된 닉네임 입니다";
+                nicknameMessage.color = Color.red;
+                nickCheckImage01.color = Color.red;
+                nickCheckImage02.color = Color.red;
+                break;
+            case 1:
+                nicknameMessage.text = "사용 가능한 닉네임 입니다";
+                nicknameMessage.color = Color.green;
+                nickCheckImage01.color = Color.green;
+                nickCheckImage02.color = Color.green;
+                break;
+        }
+    }
+#endregion
 
 
     public void IsDisplayNickNameDuplication(UnityAction OnCheck)
@@ -189,12 +269,14 @@ public class SignUpPanel : MonoBehaviour
     {
         if (passwordInputField.text.Length >= 6 && passwordInputField.text.Length <= 20)
         {
-            passwordMessage.text = "사용 가능";
+            passwordMessage.text = "사용 가능한 비밀번호 입니다";
+            passwordMessage.color = Color.green;
             passwordCheck = true;
         }
         else
         {
-            passwordMessage.text = "잘못된 비밀번호";
+            passwordMessage.text = "6 ~ 20 자리로 설정해 주세요";
+            passwordMessage.color = Color.red;
             passwordCheck = false;
         }
         SignUpInteractable();
@@ -206,12 +288,14 @@ public class SignUpPanel : MonoBehaviour
     {
         if (passwordConfirmInputField.text == passwordInputField.text)
         {
-            passwordConfirmMessage.text = "일치 합니다.";
+            passwordConfirmMessage.text = "비밀번호가 일치 합니다";
+            passwordConfirmMessage.color = Color.green;
             passwordConfirmCheck = true;
         }
         else
         {
-            passwordConfirmMessage.text = "비밀번호가 일치하지 않습니다.";
+            passwordConfirmMessage.text = "비밀번호가 일치하지 않습니다";
+            passwordConfirmMessage.color = Color.red;
             passwordConfirmCheck = false;
         }
         SignUpInteractable();
@@ -252,10 +336,25 @@ public class SignUpPanel : MonoBehaviour
         passwordConfirmCheck &&
         displayNicknameCheck
         )
-            SignUpButton.interactable = true;
+            signUpButtonChecker = 1;
         else
         {
-            SignUpButton.interactable = false;
+            signUpButtonChecker = 0;
+        }
+        StartCoroutine(SignUpInteractableCoroutine());
+    }
+
+    IEnumerator SignUpInteractableCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        switch(signUpButtonChecker)
+        {
+            case 0:
+                SignUpButton.interactable = false;
+                break;
+            case 1:
+                SignUpButton.interactable = true;
+                break;
         }
     }
 
